608f1cdee475d05a27a080bbc518b6df16030552
fadelyking <fadely195@gmail.com>
2023-07-16 16:39:53 +0300

    Connected the DOM with the array, and created the switch player function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function TicGame() {
 	// Gameboard module with a board array inside of it to store the value of each position
 	const gameBoard = (() => {
 		let i = 0;
-		let board = Array(9).fill("x");
+		let board = Array(9).fill("");
 		return board;
 	})();
 
@@ -21,37 +21,66 @@ function TicGame() {
 		const thirdRow = document.createElement("div");
 		thirdRow.classList.add("third-row");
 
-		for (let i = 0; i <= gameBoard.length - 1; i++) {
-			if (3 > i) {
-				container.appendChild(firstRow);
-				const square = document.createElement("div");
-				square.classList.add(`square-${i}`);
-				square.textContent = `${gameBoard[i]} - ${i}`;
-				firstRow.appendChild(square);
-				console.log(firstRow);
-			} else if (3 <= i && 6 > i) {
-				container.appendChild(secondRow);
-				const square = document.createElement("div");
-				square.classList.add(`square-${i}`);
-				square.textContent = `${gameBoard[i]} - ${i}`;
-				secondRow.appendChild(square);
-			} else {
-				container.appendChild(thirdRow);
-				const square = document.createElement("div");
-				square.classList.add(`square-${i}`);
-				square.textContent = `${gameBoard[i]} - ${i}`;
-				thirdRow.appendChild(square);
+		// Each row takes up to three squares
+		function generateSquares() {
+			for (let i = 0; i <= gameBoard.length - 1; i++) {
+				if (3 > i) {
+					container.appendChild(firstRow);
+					const square = document.createElement("div");
+					square.classList.add(`square-${i}`);
+					square.setAttribute("data-index", `${i}`);
+					square.textContent = `${gameBoard[i]}`;
+					firstRow.appendChild(square);
+				} else if (3 <= i && 6 > i) {
+					container.appendChild(secondRow);
+					const square = document.createElement("div");
+					square.classList.add(`square-${i}`);
+					square.textContent = `${gameBoard[i]}`;
+					square.setAttribute("data-index", `${i}`);
+					secondRow.appendChild(square);
+				} else {
+					container.appendChild(thirdRow);
+					const square = document.createElement("div");
+					square.classList.add(`square-${i}`);
+					square.textContent = `${gameBoard[i]}`;
+					square.setAttribute("data-index", `${i}`);
+					thirdRow.appendChild(square);
+				}
 			}
 		}
+		generateSquares();
+
+		// Push value to array on click
+		let activePlayer = 0;
+		const squareClick = container.addEventListener("click", (e) => {
+			console.log(activePlayer);
+			let number = e.target.getAttribute(`data-index`);
+			if (activePlayer === 0 && e.target.textContent !== "O") {
+				e.target.textContent = "X";
+				gameBoard[parseInt(number) + 1] = "x";
+
+				// Switch Active Player
+				activePlayer = activePlayer === 0 ? 1 : 0;
+			} else if (activePlayer === 1 && e.target.textContent !== "X") {
+				e.target.textContent = "O";
+				gameBoard[parseInt(number) + 1] = "O";
+
+				// Switch Active Player
+				activePlayer = activePlayer === 0 ? 1 : 0;
+			}
+			console.log(gameBoard);
+		});
 	})();
 
 	// Factory function to create players
+
+	function GameController(playerOne = Player("Fadely"), playerTwo = Player("Fadals")) {
+		const board = gameBoard();
+	}
 	const Player = (name) => {
 		const getName = () => name;
 		return { getName };
 	};
-
-	const fadely = Player("Fadely");
 }
 
 TicGame();
