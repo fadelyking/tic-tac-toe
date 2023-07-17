7a963a52604de7b305405e292097238e378d08cc
fadelyking <fadely195@gmail.com>
2023-07-17 08:11:58 +0300

    logic that checks for when the game is over! 3 in a row/tie

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 function TicGame() {
 	// Gameboard module with a board array inside of it to store the value of each position
 	const gameBoard = (() => {
-		let i = 0;
 		let board = Array(9).fill("");
 		return board;
 	})();
+
+	console.log(gameBoard);
 
 	// Display Controller
 	const displayController = (() => {
@@ -50,6 +51,72 @@ function TicGame() {
 		}
 		generateSquares();
 
+		// Winning Function
+		function winX() {
+			const body = document.querySelector("body");
+			if (
+				(gameBoard[1] === gameBoard[2] &&
+					gameBoard[2] === gameBoard[3] &&
+					gameBoard[1] === "X") ||
+				(gameBoard[4] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[6] &&
+					gameBoard[4] === "X") ||
+				(gameBoard[6] === gameBoard[7] &&
+					gameBoard[7] === gameBoard[8] &&
+					gameBoard[6] === "X") ||
+				(gameBoard[1] === gameBoard[4] &&
+					gameBoard[4] === gameBoard[7] &&
+					gameBoard[1] === "X") ||
+				(gameBoard[2] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[8] &&
+					gameBoard[2] === "X") ||
+				(gameBoard[3] === gameBoard[6] &&
+					gameBoard[6] === gameBoard[9] &&
+					gameBoard[3] === "X") ||
+				(gameBoard[1] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[9] &&
+					gameBoard[1] === "X") ||
+				(gameBoard[3] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[7] &&
+					gameBoard[3] === "X")
+			) {
+				alert("X is the winner");
+				gameBoard.fill("");
+			}
+		}
+
+		function winO() {
+			if (
+				(gameBoard[1] === gameBoard[2] &&
+					gameBoard[2] === gameBoard[3] &&
+					gameBoard[1] === "O") ||
+				(gameBoard[4] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[6] &&
+					gameBoard[4] === "O") ||
+				(gameBoard[6] === gameBoard[7] &&
+					gameBoard[7] === gameBoard[8] &&
+					gameBoard[6] === "O") ||
+				(gameBoard[1] === gameBoard[4] &&
+					gameBoard[4] === gameBoard[7] &&
+					gameBoard[1] === "O") ||
+				(gameBoard[2] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[8] &&
+					gameBoard[2] === "O") ||
+				(gameBoard[3] === gameBoard[6] &&
+					gameBoard[6] === gameBoard[9] &&
+					gameBoard[3] === "O") ||
+				(gameBoard[1] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[9] &&
+					gameBoard[1] === "O") ||
+				(gameBoard[3] === gameBoard[5] &&
+					gameBoard[5] === gameBoard[7] &&
+					gameBoard[3] === "O")
+			) {
+				alert("O is the Winner");
+				/* gameBoard.fill(""); */
+			}
+		}
+
 		// Push value to array on click
 		let activePlayer = 0;
 		const squareClick = container.addEventListener("click", (e) => {
@@ -57,7 +124,7 @@ function TicGame() {
 			let number = e.target.getAttribute(`data-index`);
 			if (activePlayer === 0 && e.target.textContent !== "O") {
 				e.target.textContent = "X";
-				gameBoard[parseInt(number) + 1] = "x";
+				gameBoard[parseInt(number) + 1] = "X";
 
 				// Switch Active Player
 				activePlayer = activePlayer === 0 ? 1 : 0;
@@ -68,6 +135,28 @@ function TicGame() {
 				// Switch Active Player
 				activePlayer = activePlayer === 0 ? 1 : 0;
 			}
+
+			// Calculate how many times X or O were clicked
+			let xCount = 0;
+			let oCount = 0;
+			for (option of gameBoard) {
+				if (option === "X") {
+					xCount++;
+				} else if (option === "O") {
+					oCount++;
+				}
+			}
+			console.log(oCount, xCount);
+			if (winX()) {
+				return winX();
+			} else if (winO()) {
+				return winO();
+				// Return tie based on the x/o count
+			} else if (xCount === 5 && oCount === 4) {
+				alert("TIE");
+			}
+			winX();
+			winO();
 			console.log(gameBoard);
 		});
 	})();
